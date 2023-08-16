f01a90c3cc4c0bd89bf068c9d482307f99ed34bb
PavleProd <pavleprod249@gmail.com>
2023-08-16 21:50:14 +0200

    sorting doctor table column

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -26,6 +26,30 @@ export class HomeComponent implements OnInit {
     this.getDoctors(this.name, this.surname, this.speciality)
   }
 
+  getImage(field: keyof Doctor) {
+    if (this.myMap.get(field) == 1) {
+      return this.sortDecreasingImage
+    }
+    return this.sortIncreasingImage
+  }
+
+  sort(field: keyof Doctor) {
+    this.myMap.set(field, this.myMap.get(field) * -1)
+    this.doctors.sort(this.sortDoctorsByField(field, this.myMap.get(field)))
+  }
+
+  sortDoctorsByField(field: keyof Doctor, order: number) {
+    if (order == 1) { // opadajuce
+      return (a: Doctor, b: Doctor) => a[field] < b[field] ? 1 : -1;
+    }
+
+    return (a: Doctor, b: Doctor) => a[field] > b[field] ? 1 : -1; // rastuce
+  }
+
+  myMap = new Map<string, number>([['name', 1], ['surname', 1], ['speciality', 1]])
+  sortDecreasingImage = "http://localhost:4000/icons/sort-down.svg"
+  sortIncreasingImage = "http://localhost:4000/icons/sort-up.svg"
+
   doctors: Doctor[]
   name: string = ""
   surname: string = ""
